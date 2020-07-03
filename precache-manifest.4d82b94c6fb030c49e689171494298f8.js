37d3a8c958ea339306edcb84a808f3b16dd68e8c
igokul95 <gokul@perleybrook.com>
2020-07-03 12:08:54 +0530

    Updates

---
+++ precache-manifest.4d82b94c6fb030c49e689171494298f8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d65d3b7b1e0e537c1d180fc5d8502e4",
+    "url": "/FindTheFalcon/index.html"
+  },
+  {
+    "revision": "f8c5bb3d41194b20f2b6",
+    "url": "/FindTheFalcon/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "1c29ac47ce571d56a951",
+    "url": "/FindTheFalcon/static/css/main.202b74f0.chunk.css"
+  },
+  {
+    "revision": "f8c5bb3d41194b20f2b6",
+    "url": "/FindTheFalcon/static/js/2.08ff1021.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/FindTheFalcon/static/js/2.08ff1021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c29ac47ce571d56a951",
+    "url": "/FindTheFalcon/static/js/main.847d78e1.chunk.js"
+  },
+  {
+    "revision": "f8db7c9cd0ed7aa8cf4c",
+    "url": "/FindTheFalcon/static/js/runtime-main.eb0cd005.js"
+  },
+  {
+    "revision": "7e244568918b596817c367c834cacbed",
+    "url": "/FindTheFalcon/static/media/Logo.7e244568.png"
+  },
+  {
+    "revision": "db7e644defab14691ac5d9b0d61fa001",
+    "url": "/FindTheFalcon/static/media/back.db7e644d.jpg"
+  }
+]);
