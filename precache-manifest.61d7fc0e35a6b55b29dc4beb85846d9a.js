3a1d14d9127adffa59554ad35561d1a63d29c1da
igokul95 <gokul@perleybrook.com>
2020-07-03 13:19:17 +0530

    Remove project files and replace with build

---
+++ precache-manifest.61d7fc0e35a6b55b29dc4beb85846d9a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25af8e0202311cd4251e7633e311f2e9",
+    "url": "/FindTheFalcon/index.html"
+  },
+  {
+    "revision": "1ad8a296869d3236e046",
+    "url": "/FindTheFalcon/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "ec859e62b550f550c836",
+    "url": "/FindTheFalcon/static/css/main.202b74f0.chunk.css"
+  },
+  {
+    "revision": "1ad8a296869d3236e046",
+    "url": "/FindTheFalcon/static/js/2.03b9e27d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/FindTheFalcon/static/js/2.03b9e27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec859e62b550f550c836",
+    "url": "/FindTheFalcon/static/js/main.1d874b06.chunk.js"
+  },
+  {
+    "revision": "f8db7c9cd0ed7aa8cf4c",
+    "url": "/FindTheFalcon/static/js/runtime-main.eb0cd005.js"
+  },
+  {
+    "revision": "7e244568918b596817c367c834cacbed",
+    "url": "/FindTheFalcon/static/media/Logo.7e244568.png"
+  },
+  {
+    "revision": "db7e644defab14691ac5d9b0d61fa001",
+    "url": "/FindTheFalcon/static/media/back.db7e644d.jpg"
+  }
+]);
